6fa1e1a4e99ef12f02c47e98c7bdbe3d93fb34fb
alex <alex@link.homenet.telecomitalia.it>
2018-04-11 01:12:35 +0200

    Versione Buggata, ma migliore

--- TestingPesante/TestingImg.py
+++ TestingPesante/TestingImg.py
@@ -18,56 +18,45 @@ def PostaInstagram():
     photo_path = '/home/alex/Documenti/LOL/C0D3rPAzZ0/Baws/Al/PyTumblr_project/TestingPesante/0.jpg'
     caption = ":)"
     oInstagramAPI.uploadPhoto(photo_path, caption=caption)
+def stampa(stringa):
+    k =1
+    num_img =0
+    first_space=0
+    
+    img = Image.open("sfondo_quotes4.jpg")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("GreatVibes-Regular.otf" , 150)
+            
+    while(first_space<len(stringa)):
+        first_space=40*k
+        if(first_space < len(stringa)): 
+            try:
+                while(first_space*k<len (stringa)):
+                    if(stringa[first_space]!=  ( (' ') or '')):
+                        first_space = first_space*k + 1 #becco il primo spazio per andare a capo
+                    else:
+                        break
+            except(IndexError):
+                print("Hai beccato na quote sfigata zio")
+            draw.text((30, 305+(130*k)),stringa[(k-1)*first_space:first_space*k],(255,255,255),font=font)#<---- Worka!
+            print(str(k) +'\t')
+
+            k +=1
+            num_img +=1
+            
+        else:
+            
+            draw.text((30, 435*k),stringa,(255,255,255),font=font)#<---- Worka!
+            
+            k +=1
+            num_img +=1
+    img.save(str(num_img)+'.jpg')
 if __name__ == '__main__':
     
     cits = get_titoli_from_reddit('quotes')
     print ("Elementi: "+ str(cits.__len__()))
-<<<<<<< HEAD
-    k =0
-    
-    for cit in cits:  
-        if(len(cit)>44):
-            s=40
-            try:
-                while(cit[s]!=   (' ') or ''):
-                    s+=1
-            except(IndexError):
-                print("Hai beccato na quote sfigata zio")
-            
-            img = Image.open("sfondo_quotes4.jpg")
-            draw = ImageDraw.Draw(img)
-            font = ImageFont.truetype("GreatVibes-Regular.otf" , 150)
-            draw.text((30, 435),cit[0:s],(255,255,255),font=font)#<---- Worka!
-            draw.text((30, 595),cit[s:],(255,255,255),font=font)
-            img.save(str(k)+'.jpg')
-            k +=1
-=======
-    k =1
-    
-    for cit in cits:  
-        if(len(cit)>44):#per le cazzo di quotes kilometiche
-            first_space=40
-            if(first_space * k < len(cit)): 
-                try:
-                    while(cit[first_space*k]!=   (' ') or ''):
-                        first_space = first_space*k + 1 #becco il primo spazio per andare a capo
-                except(IndexError):
-                    print("Hai beccato na quote sfigata zio")
-                img = Image.open("sfondo_quotes4.jpg")
-                draw = ImageDraw.Draw(img)
-                font = ImageFont.truetype("GreatVibes-Regular.otf" , 150)
-                draw.text((30, 435),cit[(k-1)*first_space:first_space*k],(255,255,255),font=font)#<---- Worka!
-                draw.text((30, 595),cit[first_space*k:],(255,255,255),font=font)
-                img.save(str(k)+'.jpg')
-                k +=1
->>>>>>> Porco dio mancava la commit iniziale
-        else:
-            img = Image.open("sfondo_quotes.jpg")
-            draw = ImageDraw.Draw(img)
-            font = ImageFont.truetype("GreatVibes-Regular.otf" , 72)
-            draw.text((30, 475),cit,(0,0,0),font=font)#<---- Worka!
-            img.save(str(k)+'.jpg')
-            k +=1
+    for k in cits:
+        stampa(k)
     #PostaInstagram()
             
     
